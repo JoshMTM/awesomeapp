06a8b8c25b7aecfa13be9af70d6b0f10b610e553
JoshMTM <joshmitima08@yahoo.com>
2021-09-05 17:22:21 +0200

    test react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React is now starting !!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Josh first step with React
         </a>
       </header>
+      <body>
+        <h1>This is gonna be fun</h1>
+      </body>
     </div>
   );
 }
